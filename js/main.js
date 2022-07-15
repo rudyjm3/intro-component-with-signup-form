7e141d88720fba95468491ae106bef5ecf7030ee
rudyjm3 <rudyjm3@yahoo.com>
2022-07-15 03:03:44 -0400

    Added some variables to main js file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const mainForm = document.getElementById('trial-form');
+const errorTxt = document.getElementsByClassName('error-txt');
+const errorIcon = document.getElementsByClassName('error-icon');
+console.log(errorTxt);
+console.log(errorIcon);
